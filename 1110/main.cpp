4a2cb25ecad399423e55530c264e7fc61b24284c
HuaMa001 <67962882+HuaMa001@users.noreply.github.com>
2025-11-10 17:27:07 +0800

    題目3 PC1合併

--- 1110/main.cpp
+++ 1110/main.cpp
@@ -1,3 +1,10 @@
+<<<<<<< Updated upstream
+=======
+#include <QApplication>
+#include <QMainWindow>
+#include <QMdiArea>
+#include <QMdiSubWindow>
+>>>>>>> Stashed changes
 #include "mainwindow.h"
 
 #include <QApplication>
@@ -5,7 +12,30 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+<<<<<<< Updated upstream
     MainWindow w;
     w.show();
+=======
+
+    QMainWindow mainWin;
+    mainWin.setWindowTitle("兩個視窗水平排列");
+
+    QMdiArea *mdi = new QMdiArea;
+    mainWin.setCentralWidget(mdi);
+
+    MainWindow *w1 = new MainWindow;
+    MainWindow *w2 = new MainWindow;
+
+    QMdiSubWindow *sub1 = mdi->addSubWindow(w1);
+    QMdiSubWindow *sub2 = mdi->addSubWindow(w2);
+
+    sub1->show();
+    sub2->show();
+
+    mdi->tileSubWindows(); // 自動排列水平或垂直
+
+    mainWin.show();
+
+>>>>>>> Stashed changes
     return a.exec();
 }
--- 1110/mainwindow.cpp
+++ 1110/mainwindow.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPushButton>
+#include <QVBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // 建立按鈕
+    QPushButton *button = new QPushButton("按鈕", this);
+
+    // 建立垂直布局，放到 centralwidget
+    QVBoxLayout *layout = new QVBoxLayout(ui->centralwidget);
+
+    // 上下拉伸 + 水平置中
+    layout->addStretch();
+    layout->addWidget(button, 0, Qt::AlignHCenter);
+    layout->addStretch();
 }
 
 MainWindow::~MainWindow()
